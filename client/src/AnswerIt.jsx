595ca6bef617d59e998841c362a05f30214e6d3f
JeevanandhiniN <jeevanandhini12@gmail.com>
2020-11-23 03:39:43 -0800

    Fixed sort and pagination issues

--- client/src/AnswerIt.jsx
+++ client/src/AnswerIt.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-var AnswerIt = ({answer, onChangeAns, ansScrNm, onChangeAnsScrNm, handleCancel, submitAnswer}) => {
+var AnswerIt = ({ handleInputChangeAns, handleCancel, submitAnswer}) => {
   // <div className ='que-line2'>Answers must be at least 20 characters long </div>
 
   return (
     <div className='ans-text'>
             <div className='bold'> your answer </div>
-          <div><textarea className ='text'  value = {answer} onChange ={onChangeAns}> </textarea></div>
+          <div><textarea className ='text' onChange ={(e) => {handleInputChangeAns(e, 'answer')}} name ='answer'> </textarea></div>
 
           <div className ='que-line2'>at least 20 characters</div>
           <div className ='que-line2'> screen name </div>
-          <div><textarea className ='ScrName'  value = {ansScrNm} onChange ={onChangeAnsScrNm}> </textarea></div>
+          <div><textarea className ='ScrName'  onChange ={(e) => {handleInputChangeAns(e, 'ansScrNm')}} name ='ansScrNm'> </textarea></div>
           <div className ='que-line2'> this name will be displayed with your answer</div>
           <div className ='que-line2'> by submitting I agree to the q&a guidelines</div>
           <div>
